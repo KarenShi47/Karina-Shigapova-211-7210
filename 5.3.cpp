f15cff34b48fe1d14a2fbcf33abb5ee4677aab48
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-14 16:02:39 +0300

    Create 5.3.cpp

---
+++ 5.3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double A, B, C, AC, BC, x;
+    cout << "Введите A, B, C" << endl;
+    cin >> A >> B >> C;
+    if ((A > C and C > B) or (A < C and C < B))
+        {
+        AC = abs(A - C);
+        BC = abs(B - C);
+        x = AC*BC;
+        cout << "AC = " << AC << endl << "BC = " << BC << endl;
+        cout << "AC*BC = " << x;
+
+        }
+
+     else
+        {
+        cout << "не может быть по условию";
+        }
+
+        return 0;
+}
