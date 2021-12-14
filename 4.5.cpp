95565dbfe8afe041143656316b4c1453321d4c9b
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-14 15:54:00 +0300

    Update 4.5.cpp

--- 4.5.cpp
+++ 4.5.cpp
@@ -9,8 +9,8 @@ int main()
     setlocale(LC_ALL, "Russian");
     cout << "Введите a и b" << endl;
     cin >> a >> b;
-    cout << "Сумма =" << abs(a) + abs(b) << endl << "Разность =" << abs(a) - abs(b) << endl; 
-    cout << "Произведение =" << abs(a)*abs(b) << endl << "Частное =" << abs(a)/abs(b); 
+    cout << "Сумма = " << abs(a) + abs(b) << endl << "Разность = " << abs(a) - abs(b) << endl; 
+    cout << "Произведение = " << abs(a)*abs(b) << endl << "Частное = " << abs(a)/abs(b); 
        
        return 0;
 }
