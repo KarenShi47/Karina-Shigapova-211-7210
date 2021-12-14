703c37f28c364ab883ec5b355ea489e2c3b159c6
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-15 02:35:16 +0300

    Create 11.1.cpp

---
+++ 11.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+	cout << "Введите значения А и В" << endl;
+	cin >> a >> b;
+
+	if (a != b) {
+		a = a + b;
+		b = a;
+		cout << " А = " << a << endl << " В = " << b << endl;
+	}
+	else {
+		a = b = 0;
+		cout << " A = " << a << endl << " B = " << b << endl;
+	}
+	 return 0;
+}
