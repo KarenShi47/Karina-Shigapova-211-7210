acff982ba50fa1b857c8cccd6cdd2772e93870b3
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-14 17:30:42 +0300

    Update 6.6.cpp

--- 6.6.cpp
+++ 6.6.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double A, x;
+	cout << "Введите A" << endl;
+	cin >> A;
+	x = pow(A, 2) * pow(A, 2) * pow(A, 2);
+	cout << "A^8 = " << x;
+
+
+	return 0;
+
+}
