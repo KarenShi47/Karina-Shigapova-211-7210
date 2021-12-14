e611141aaf9196bb4f586480d078f9c903fc64ff
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-14 15:38:27 +0300

    Create 4.5.cpp

---
+++ 4.5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    double a, b;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите a и b" << endl;
+    cin >> a >> b;
+    cout << "Сумма =" << abs(a) + abs(b) << endl << "Разность =" << abs(a) - abs(b) << endl; 
+    cout << "Произведение =" << abs(a)*abs(b) << endl << "Частное =" << abs(a)/abs(b); 
+       
+       return 0;
+}
