19fe8f622db16f259be95dfcc375dd4f3c98e2a2
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-14 15:53:46 +0300

    Update 4.4.cpp

--- 4.4.cpp
+++ 4.4.cpp
@@ -9,8 +9,8 @@ int main()
     double a, b;
     cout << "Введите a и b" << endl;
     cin >> a >> b;
-    cout << "Сумма =" << a*a + b*b << endl << "Разность =" << a*a - b*b << endl;
-    cout << "Произведение =" << (a*a)*(b*b) << endl << "Частное =" << (a*a)/(b*b);
+    cout << "Сумма = " << a*a + b*b << endl << "Разность = " << a*a - b*b << endl;
+    cout << "Произведение = " << (a*a)*(b*b) << endl << "Частное = " << (a*a)/(b*b);
 
        return 0;
 }
