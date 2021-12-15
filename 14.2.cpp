0f99a3bee08c1bdc5abb3f123d002c3c15d47ac1
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-15 17:39:08 +0300

    Create 14.2.cpp

---
+++ 14.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{   setlocale(LC_ALL, "Russian");
+    int a, n, x, y, i, b;
+    cout << "Введите числа А и В (A > B)" << endl;
+            cin >> a >> n;
+            for (; a - n >= 0; a -= n) {}
+            cout << "Длина незанятой части отрезка А равна: " << a;
+        }
