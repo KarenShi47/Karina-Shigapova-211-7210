f24aaca1e84ad866971d43effdffe17cb2885799
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-23 20:04:37 +0300

    Create 16.3.cpp

---
+++ 16.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+
+
+int main()
+
+{
+	int n, a, b, x = 0;
+	cin >> n >> a >> b;
+	int *arr = new int[n];
+	arr[0] = a;
+	arr[1] = b;
+	cout << arr[0] << endl;
+	cout << arr[1] << endl;
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = 0; j < i + 2; j++)
+		{
+			x = x + arr[j];
+		}
+		arr[i + 2] = x;
+		x = 0;
+		cout << arr[i + 2] << endl;
+	}
+    delete[] arr;
+	return 0;
+}
