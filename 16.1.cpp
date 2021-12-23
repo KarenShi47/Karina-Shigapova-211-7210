725ef40f8bcc062f9671d49ceb7eae1d2f5575b2
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-23 19:44:16 +0300

    Create 16.1.cpp

---
+++ 16.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+
+int main()
+
+{
+	int n, x = 1;
+	cin >> n;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = x;
+		x = x + 2;
+		cout << arr[i] << endl;
+	}
+    delete[] arr;
+	return 0;
+}
