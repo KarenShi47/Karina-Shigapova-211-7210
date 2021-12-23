dbf960734560c926bb0d920f4e454d72aed60932
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-23 19:01:12 +0300

    Create 15.2.cpp

---
+++ 15.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+
+int Sign(float x)
+{
+    if (x < 0)
+    {
+        return -1;
+    }
+    else if (x == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+
+int main()
+
+{
+    float a, b;
+    cin >> a >> b;
+    cout << Sign(a) + Sign(b);
+    return 0;
+}
