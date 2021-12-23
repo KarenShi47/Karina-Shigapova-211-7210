4d94156c97fb7fd2467d16e009668263e29409b8
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-23 19:10:10 +0300

    Create 15.3.cpp

---
+++ 15.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+
+float RingS(float r1, float r2)
+{
+    return 3.14 * r1 * r1 - 3.14 * r2 * r2;
+}
+
+
+int main()
+
+{
+    float r1, r2, r3, r4, r5, r6;
+    cin >> r1 >> r2 >> r3 >> r4 >> r5 >> r6;
+    cout << RingS(r1, r2) << endl;
+    cout << RingS(r3, r4) << endl;
+    cout << RingS(r5, r6) << endl;
+    return 0;
+}
