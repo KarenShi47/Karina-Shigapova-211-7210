d17bc4ed30eea6872ca6b4254169fa729f6c4234
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2021-12-23 20:37:51 +0300

    Create 16.5.cpp

---
+++ 16.5.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+
+int main()
+
+{
+	int n;
+	cin >> n;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+	    arr[i] = i + 1;
+	}
+	for (int i = 0; i < n; i = i + 2)
+    {
+		cout << arr[i] << endl;
+	}
+	if (n % 2 == 1)
+	{
+		n = n - 1;
+	}
+	for (int i = n - 1; i >= 0; i = i - 2)
+	{
+		cout << arr[i] << endl;
+	}
+    delete[] arr;
+	return 0;
+}
