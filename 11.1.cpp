d381d6ab148d99d227d7e29a8d797c9c392ece32
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2022-01-20 15:03:04 +0300

    Update 11.1.cpp

--- 11.1.cpp
+++ 11.1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 
 using namespace std;
 
@@ -19,4 +19,29 @@ int main()
 		cout << " A = " << a << endl << " B = " << b << endl;
 	}
 	 return 0;
+} */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "Введите значения А и В" << endl;
+	cin >> a >> b;
+
+	if (a != b)
+	{
+		a = max(a, b);
+		b = a;
+		cout << " А = " << a << endl << " В = " << b << endl;
+	}
+	else
+	{
+		a = b = 0;
+		cout << " A = " << a << endl << " B = " << b << endl;
+	}
+	return 0;
 }
