d9b192aa0e0304c05bf85c705ca642149f21302a
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2022-01-22 14:03:39 +0300

    Update 16.1.cpp

--- 16.1.cpp
+++ 16.1.cpp
@@ -5,15 +5,17 @@ using namespace std;
 int main()
 
 {
+	setlocale(LC_ALL, "Russian");
 	int n, x = 1;
+	cout << "Введите число" << endl;
 	cin >> n;
-	int *arr = new int[n];
+	int* arr = new int[n];
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = x;
 		x = x + 2;
 		cout << arr[i] << endl;
 	}
-    delete[] arr;
+	delete[] arr;
 	return 0;
 }
