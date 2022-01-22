f3fe7e24ad5a4b03988bd48a3e2599cb9985ea37
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2022-01-22 14:06:13 +0300

    Update 16.2.cpp

--- 16.2.cpp
+++ 16.2.cpp
@@ -5,15 +5,17 @@ using namespace std;
 int main()
 
 {
+	setlocale(LC_ALL, "Russian");
 	int n, a, d;
+	cout << "Введите числа" << endl;
 	cin >> n >> a >> d;
-	int *arr = new int[n];
+	int* arr = new int[n];
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = a;
 		a = a * d;
 		cout << arr[i] << endl;
 	}
-    delete[] arr;
+	delete[] arr;
 	return 0;
 }
