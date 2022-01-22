02cf24539c04c929c660f312854420993ca6f952
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2022-01-22 13:26:39 +0300

    Update 15.1.cpp

--- 15.1.cpp
+++ 15.1.cpp
@@ -12,6 +12,8 @@ float PowerA3(float A, float B)
 int main()
 
 {
+    setlocale(LC_ALL, "Russian");
+    cout << "Описать функцию PowerA3, вычисляющую 3ю степень чисел от 1 до 5" << endl;
     cout << PowerA3(1, 0) << endl;
     cout << PowerA3(2, 0) << endl;
     cout << PowerA3(3, 0) << endl;
