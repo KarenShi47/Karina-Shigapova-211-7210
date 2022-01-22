1bc48710f572cebea5a36f1d6cdbfe312478a31c
KarenShi47 <90842118+KarenShi47@users.noreply.github.com>
2022-01-22 14:00:54 +0300

    Update 15.5.cpp

--- 15.5.cpp
+++ 15.5.cpp
@@ -1,4 +1,4 @@
-#include <iostream> 
+/*#include <iostream> 
 using namespace std;
 
 
@@ -28,33 +28,40 @@ int main()
 
 {
     return 0;
-}
-#include <iostream> 
+}*/
+
+#include <iostream>
+#include <math.h>
+
 using namespace std;
 
 
-float Fact2(int n)
+
+double Fact2(int n) 
 {
-    double Fact2(int n);
-    {
-        double ans, i;
-        if (n % 2 == 0)
-            ans = 2;
-        else
-            ans = 1;
-        for (i = ans + 2; i <= n; i += 2) {
-            ans *= i;
-        }
-        return ans;
+    double a, i;
+    if (n % 2 == 0)
+        a = 2;
+    else
+        a = 1;
+    for (i = a + 2; i <= n; i += 2) {
+        a *= i;
     }
+    return a;
 }
 
-
 int main()
-
 {
-    cout << "Введите целое значение N:\n";
-    cin >> n;
-    a = Fact2(n);
-    cout << "Двойной факториал числа N равен: " << a << "\n\n";
+    setlocale(LC_ALL, "Russian");
+    int a, n;
+    double x, b;
+        {
+            cout << "Введите целое значение N:" << endl;
+            cin >> n;
+            x = Fact2(n);
+            cout << "Двойной факториал числа N равен: " << x << endl;
+        }
+
+        
+    return 0;
 }
